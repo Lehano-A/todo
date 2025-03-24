dd5f1f536294a38b6c696035f8cfd7507c52d3cd
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-03-24 22:53:56 +0300

    Добавляет кнопку создания новой задачи

--- src/components/Main/Columns/Todo/Todo.tsx
+++ src/components/Main/Columns/Todo/Todo.tsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import AddIcon from '../../../../images/icons/add.svg'
 import Column from '../../../../styled/column'
 import Task from '../../Task/Task'
 
 const StyledTodo = styled(Column)`
   background-color: #8080800f;
+  position: relative;
+`
+
+const StyledAddIcon = styled('svg')`
+  background: url(${AddIcon}) no-repeat center center;
+  width: 100%;
+  height: 100%;
+  filter: invert(51%) sepia(100%) saturate(327%) hue-rotate(100deg) brightness(95%) contrast(86%);
+`
+
+const AddTask = styled('button')`
+  width: 40px;
+  height: 40px;
+  background-size: 40px 40px;
+  background-color: white;
+  cursor: pointer;
+  border: none;
+  border-radius: 50%;
+  position: absolute;
+  bottom: 25px;
+  padding: 0;
 `
 
 function Todo() {
   return (
     <StyledTodo>
       <Task>DSFSDFDSFSDF</Task>
+
+      <AddTask>
+        <StyledAddIcon></StyledAddIcon>
+      </AddTask>
     </StyledTodo>
   )
 }
