e54b96aa963aae8c2db7e249c91684111b75723e
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-05 02:10:27 +0300

    Добавляет кнопку удаления задачи

--- src/components/Main/Task/Task.tsx
+++ src/components/Main/Task/Task.tsx
@@ -1,21 +1,49 @@
 import React, { PropsWithChildren } from 'react'
 import styled from 'styled-components'
 
+import { ReactComponent as IconDelete } from '../../../images/icons/delete.svg'
+
 const StyledTask = styled('div')`
+  position: relative;
   width: 300px;
   background-color: white;
   padding: 20px;
   border-radius: 12px;
+
+  &:hover {
+    & #buttonDeleteTask {
+      visibility: visible;
+      opacity: 1;
+    }
+  }
+`
+
+const StyledIconDelete = styled(IconDelete)`
+  position: absolute;
+  top: 5px;
+  right: 5px;
 `
 
 const Title = styled('h2')`
   font-size: 1rem;
 `
 
+const ButtonDelete = styled('button')`
+  width: 20px;
+  height: 20px;
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+`
+
 function Task({ children }: PropsWithChildren) {
   return (
     <StyledTask>
       <Title>{children}</Title>
+
+      <ButtonDelete id='buttonDeleteTask'>
+        <StyledIconDelete />
+      </ButtonDelete>
     </StyledTask>
   )
 }
