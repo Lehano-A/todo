3f868c4b762f0e3e33759e2c2d3178b7097253fd
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-04 18:41:24 +0300

    Добавляет api для управления задачами в локальном хранилище

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,40 @@
+const ls = {
+  get: function (key: string) {
+    return localStorage.getItem(key)
+  },
+
+  add: function (key: string, value: any) {
+    const parsedData = parsingDataFromLS<string>(key)
+
+    // если данные внутри ЛХ это - массив
+    if (Array.isArray(parsedData)) {
+      parsedData.push(value)
+      localStorage.setItem(key, JSON.stringify(parsedData))
+      return
+    }
+
+    if (!parsedData) localStorage.setItem(key, JSON.stringify([value]))
+  },
+
+  remove: function (key: string, value: string) {
+    const parsedData = parsingDataFromLS<string>(key)
+
+    // если данные внутри ЛХ это - массив
+    if (Array.isArray(parsedData)) {
+      const filtered = parsedData.filter((item) => item !== value)
+      localStorage.setItem(key, JSON.stringify(filtered))
+    }
+  },
+}
+
+function parsingDataFromLS<T>(key: string): T | null {
+  const data = localStorage.getItem(key)
+
+  if (data) {
+    return JSON.parse(data)
+  }
+
+  return null
+}
+
+export default ls
