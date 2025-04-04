71e3029f1ccd22599697c2b4419b0a455915d175
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-05 00:13:04 +0300

    Добавляет логику визуализации списка задач из локального хранилища

--- src/components/Main/Columns/Todo/Todo.tsx
+++ src/components/Main/Columns/Todo/Todo.tsx
@@ -1,23 +1,26 @@
-import React, { useRef } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
 
 import AddIcon from '../../../../images/icons/add.svg'
 import { activateForm } from '../../../../redux/reducers/slices/formAddTaskSlice'
 import Column from '../../../../styled/column'
+import ls from '../../../../utils/localStorage'
 import Task from '../../Task/Task'
+import { TaskType } from '../../Task/task.type'
 
 const StyledTodo = styled(Column)`
   background-color: #8080800f;
   position: relative;
-`
 
+  :where(& > div):not(:last-of-type) {
+    margin-bottom: 10px;
+  }
+`
 const StyledAddIcon = styled('svg')`
   background: url(${AddIcon}) no-repeat center center;
   width: 100%;
   height: 100%;
-  // filter: invert(51%) sepia(100%) saturate(327%) hue-rotate(100deg) brightness(95%) contrast(86%);
 `
 
 const ButtonAddTask = styled('button')`
@@ -29,31 +32,36 @@ const ButtonAddTask = styled('button')`
   position: absolute;
   bottom: 25px;
 `
-interface FormAddTaskState {
-  formAddTask: {
-    isActive: boolean
-  }
-}
 
 function Todo({ refFormAddTask }: any) {
   const dispatch = useDispatch()
-  const isActiveFormAddTask = useSelector((state: FormAddTaskState) => state.formAddTask.isActive)
 
-  function handleFormAddTask(e: any) {
-    // console.log('e', e.currentTarget)
-    // e.currentTarget.showModal()
+  const [tasksFromLS, setTasksFromLS] = useState<TaskType[]>([])
 
+  useEffect(() => {
+    const data = ls.get('tasks')
+
+    if (Array.isArray(data)) {
+      setTasksFromLS(data)
+    }
+  }, [])
+
+  // показать форму добавления новой задачи
+  function showFormAddNewTask() {
     if (refFormAddTask.current) {
       refFormAddTask.current.showModal()
     }
 
     dispatch(activateForm())
   }
+
   return (
     <StyledTodo>
-      <Task>DSFSDFDSFSDF</Task>
+      {tasksFromLS.map((task, id) => (
+        <Task key={id + task.nameTask}>{task.nameTask}</Task>
+      ))}
 
-      <ButtonAddTask onClick={handleFormAddTask}>
+      <ButtonAddTask onClick={showFormAddNewTask}>
         <StyledAddIcon></StyledAddIcon>
       </ButtonAddTask>
     </StyledTodo>
---
+++ src/components/Main/Task/task.type.ts
@@ -0,0 +1,5 @@
+export interface TaskType {
+  nameTask: string
+  description: string
+  deadline: string
+}
