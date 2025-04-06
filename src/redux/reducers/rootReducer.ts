206c34e97fbcb9f9b1255f780dd92a058eaf3acd
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-07 01:14:32 +0300

    Добавляет todoTasksSlice в store

--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit'
 
 import formAddTaskSlice from './slices/formAddTaskSlice'
+import todoTasksSlice from './slices/todoTasksSlice'
 
-const rootReducer = combineReducers({ formAddTask: formAddTaskSlice })
+const rootReducer = combineReducers({ todoTasks: todoTasksSlice, formAddTask: formAddTaskSlice })
 
 export default rootReducer
