edea0f933a99c2919631726c0143288185836401
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-04-07 01:15:14 +0300

    Правит стиль общего стилизованного компонента Column

--- src/styled/column.ts
+++ src/styled/column.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 
 const Column = styled('div')`
-  width: 400px;
-  height: 600px;
+  width: 360px;
+  min-height: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 70px 30px 40px;
 `
 
 export default Column
