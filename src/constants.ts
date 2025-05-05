03301380bbbd1bf9f477975f58b84852f32032b2
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-05-05 11:32:41 +0300

    Обновляет constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,10 @@
-const TODO_TASKS = 'todoTasks'
 const TODO_COLUMN_NAME = 'todo'
 const INPROCESS_COLUMN_NAME = 'inProcess'
 const DONE_COLUMN_NAME = 'done'
 const ALL_TASKS = 'tasks'
 
-export { TODO_TASKS, ALL_TASKS, TODO_COLUMN_NAME, INPROCESS_COLUMN_NAME, DONE_COLUMN_NAME }
+export const DIALOG_ADD_NEW_TASK = 'dialogAddNewTask'
+export const DIALOG_EDIT_TASK = 'dialogEditTask'
+export const DIALOG_REMOVE_TASK = 'dialogRemoveTask'
+
+export { ALL_TASKS, TODO_COLUMN_NAME, INPROCESS_COLUMN_NAME, DONE_COLUMN_NAME }
