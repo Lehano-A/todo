d9f0a4025912be8be9e4a72b8ff2a6cc4825a12b
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-05-05 11:34:28 +0300

    Добавляет в store мидлвэр сохранения задач в ls

---
+++ src/redux/middlewares/saveTasksToLSMiddleware.ts
@@ -0,0 +1,34 @@
+import { Middleware } from '@reduxjs/toolkit'
+
+import ls from '../../utils/localStorage'
+import { isAction } from '../types/actions/actions.types'
+
+const actionsWithSaveToLS = ['tasks/transferTask', 'tasks/addNewTask', 'tasks/removeTask', 'tasks/editTask']
+
+// сохранить задачи в localStorage
+export const saveTasksToLSMiddleware: Middleware = (store) => (next) => (action) => {
+  // проверяем тип action
+  if (isAction(action)) {
+    // проверяем экшены, после которых нужно сохранить данные в LS
+    if (actionsWithSaveToLS.includes(action.type)) {
+      if (process.env.NODE_ENV === 'development') {
+        showInConsole(action)
+      }
+
+      const result = next(action) // сначала пропускаем экшен через редьюсеры
+      ls.saveTasks(store.getState().tasks)
+      // localStorage.setItem('tasks', JSON.stringify(store.getState().tasks))
+
+      return result
+    }
+  }
+
+  // сюда доходят экшены, для которых не нужна дополнительная логика
+  return next(action)
+}
+
+function showInConsole(action: unknown) {
+  console.group('middleware => Сохраняем в localStorage:')
+  console.log('action: ', action)
+  console.groupEnd()
+}
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import { saveTasksToLSMiddleware } from './middlewares/saveTasksToLSMiddleware'
 import rootReducer from './reducers/rootReducer'
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saveTasksToLSMiddleware),
 })
 
 export default store
 export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = typeof store.dispatch
