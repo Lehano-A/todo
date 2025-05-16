b2a1aa36d726579763c5cb99638ea53eee668df5
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2025-05-16 15:32:11 +0300

    Добавляет зависимость эффекту в хуке useCalcHeightTask, чтобы при изменении размера окна, происходил пересчёт высоты компонента задачи

--- src/hooks/useCalcHeightTask.ts
+++ src/hooks/useCalcHeightTask.ts
@@ -1,7 +1,9 @@
 import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 import { TaskElementsRefs, TaskProps } from '../components/Main/Task/task.types'
 import { CalсHeightTask, calcHeightTask } from '../components/Main/Task/utils/calcHeightTask'
+import { RootState } from '../redux/store'
 
 interface UseCalcHeightTaskProps {
   data: TaskProps['data']
@@ -20,6 +22,8 @@ export function useCalcHeightTask({
 }: UseCalcHeightTaskProps) {
   const { refTask, refContentBox } = refs
 
+  const didWindowResize = useSelector((state: RootState) => state.common.didWindowResize)
+
   useEffect(() => {
     // после монтирования компонента, без document.fonts.ready.then, вычисляется некорректная высота элемента h2, поэтому, чтобы вычисления происходили наверняка после установки всех стилей - используется document.fonts.ready.then
     document.fonts.ready.then(() => {
@@ -32,5 +36,5 @@ export function useCalcHeightTask({
     if (refTask.current && refContentBox.current && typeof styleParamsTask.closed.task.height === 'number') {
       calcHeightTask({ refs, styleParamsTask, setStyleParamsTask })
     }
-  }, [wasToggledButtonShowContent, data.nameTask, data.description])
+  }, [wasToggledButtonShowContent, data.nameTask, data.description, didWindowResize])
 }
